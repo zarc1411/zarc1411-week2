3a1398f463b4d2899ae2b906b7abdbada0a8decf
zarc1411 <zarc1483@gmail.com>
2019-12-26 13:03:35 +0530

    Tic Tac Toe

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/main.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
